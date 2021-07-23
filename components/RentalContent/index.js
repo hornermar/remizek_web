0d1a45bc09e20d63f910e338e3562032fd541391
Markéta Hornerová <hornerova.m@gmail.com>
2021-07-23 20:39:05 +0200

    obcan Javor nahozeny

--- components/RentalContent/index.js
+++ components/RentalContent/index.js
@@ -4,7 +4,8 @@ const RentalContent = () => {
       <h1>Rental</h1>
       <p>Techniku půjčujeme dle platného ceníku pouze na nekomerční použití.</p>
       <p>
-        Cenu je možné taky splatit prací v remízku, sazba je fiktivních 150 Kč/h
+        Cenu je možné taky splatit prací v remízku, sazba je fiktivních 150
+        Kč/h.
       </p>
     </div>
   );
--- pages/projekty/javor.js
+++ pages/projekty/javor.js
@@ -1,9 +1,186 @@
 //       vysázet: https://docs.google.com/document/d/15_k7zcxnQQZ4r_vSqBGErlpNlKPXVscv/edit
 
+import Link from 'next/link';
+
 const Javor = () => {
   return (
     <div>
-      <h1>Občan Javor</h1>
+      <div>
+        <h1>Občan Javor</h1>
+        <p>
+          Aktualizovaný playlist videí z dílny Remízek k Javoru Stříbrnému na
+          Anenském trojúhelníku
+        </p>
+        <p>SPOT</p>
+        <Link href="https://www.facebook.com/zachranjavor/videos/1610047322526542/">
+          https://www.facebook.com/zachranjavor/videos/1610047322526542/
+        </Link>
+        <p>Reportáž - NEDEJ SE:</p>
+        <Link href="https://www.ceskatelevize.cz/porady/1095913550-nedej-se/221562248430015-zachrana-javoru/?fbclid=IwAR0E2MwfjaiF2oUGF8ziZrJJeHhI5rOCxjSahiKID4ZFoRTijoum6ai28IY">
+          https://www.ceskatelevize.cz/.../221562248430015.../…
+        </Link>
+        <p>Strom roku:</p>
+      </div>
+
+      <div>
+        <p>Remízek audiovizuální projekty k Javoru na Smetanově nábřeží </p>
+        <h2>REPORTÁŽE</h2>
+        <p>(Remízek feat. Zachraň Javor) </p>
+
+        <div>
+          <h3>Díl nultý / Poslední nádech svobody</h3>
+          <Link href="https://fb.watch/4v2d_ZIPC5/ ">
+            https://fb.watch/4v2d_ZIPC5/
+          </Link>
+          <Link href="https://youtu.be/CsVcfmd80Po">
+            https://youtu.be/CsVcfmd80Po
+          </Link>
+          <p>/ Kamera, střih: Kryštof /</p>
+        </div>
+
+        <div>
+          <h3>Díl první / Přípravy na kácení </h3>
+          <Link href="https://fb.watch/4v3rxmYkH-/">
+            https://fb.watch/4v3rxmYkH-/
+          </Link>
+          <Link href="https://youtu.be/sDWskVhi3c8">
+            https://youtu.be/sDWskVhi3c8
+          </Link>
+          <p>
+            / Kamera: Jan Trnka, Zvuk: Kryštof, Střih: Kryštof, Marcel Halcin,
+            Juliana Moska /
+          </p>
+        </div>
+
+        <div>
+          <h3>Díl druhý / Bagr vs. Holub</h3>
+          <Link href="https://fb.watch/4v3xekd2bT/">
+            https://fb.watch/4v3xekd2bT/
+          </Link>
+          <Link href="https://youtu.be/wsMl3SRjnMc">
+            https://youtu.be/wsMl3SRjnMc
+          </Link>
+          <p>
+            / Kamera: Juliana Moska, Adéla Kaiserová, Kryštof, Zvuk: Adéla
+            Kaiserová, Matěj Lindner Střih: Rudi Mašata, Kryštof /
+          </p>
+          <img src="/javor_3.jpg" />
+        </div>
+
+        <div>
+          <h3>Díl třetí / Otevřená radnice</h3>
+          <Link href="https://fb.watch/4Oxk_K1vbK/">
+            https://fb.watch/4Oxk_K1vbK/
+          </Link>
+          <img src="/javor_4.jpg" />
+          <img src="/javor_5.jpg" />
+          <img src="/javor_6.jpg" />
+          <img src="/javor_7.jpg" />
+        </div>
+      </div>
+
+      <div>
+        <h2>ŽIVÁ VYSÍLÁNÍ</h2>
+        <div>
+          <h3>Remízek živě: Bagrování Anenského trojúhelníku</h3>
+          <Link href="https://fb.watch/4v3sy6rND0/">
+            https://fb.watch/4v3sy6rND0/
+          </Link>
+          <p>
+            / AD: Rudi Mašata, Kamera: Kryštof, Rudi Mašata, Přenos: Kryštof /
+          </p>
+        </div>
+
+        <div>
+          <h3>Remízek živě: Hudba pro Javor</h3>
+          <Link href="https://fb.watch/4v3IRv-MRm/">
+            https://fb.watch/4v3IRv-MRm/
+          </Link>
+
+          <p>
+            / Kamera: Jan Trnka Nora Štrbová Eduard Germis Juliana Moska ,
+            Produkce: Kryštof David , Živý střih, přenos: Kryštof, Stanislav
+            Miler , Zvuk: Dejv Prochazka , Světla: Aeaeæ OEn, Asistence: Anežka
+            Fišárková Adéla Kaiserová /
+          </p>
+
+          <img src="/javor_8.jpg" />
+          <img src="/javor_9.jpg" />
+          <img src="/javor_10.jpg" />
+          <img src="/javor_11.jpg" />
+          <img src="/javor_12.jpg" />
+          <img src="/javor_13.jpg" />
+          <img src="/javor_14.jpg" />
+        </div>
+
+        <div>
+          <h3>Na slovo u Javoru</h3>
+          <Link href="https://www.facebook.com/TPSremizek/videos/935027017234833 ">
+            https://www.facebook.com/TPSremizek/videos/935027017234833
+          </Link>
+
+          <img src="/javor_15.jpg" />
+          <img src="/javor_16.jpg" />
+          <img src="/javor_17.jpg" />
+          <img src="/javor_18.jpg" />
+          <img src="/javor_19.jpg" />
+          <img src="/javor_20.jpg" />
+          <img src="/javor_21.jpg" />
+          <img src="/javor_22.jpg" />
+        </div>
+
+        <div>
+          <h3>Remízek živě: Mimořádné zasedání zastupitelstva</h3>
+          <p>
+            / Kamera: Jan Trnka, Michal Vasiľ, Adéla Kaiserová, Kryštof, Živý
+            střih: Miky Arsenjev, Tereza Schrijversová, Adéla Kaiserová, Přenos:
+            Kryštof, Zvuk: Nicolas Atcheson/
+          </p>
+          <Link href="https://fb.watch/4v3OxhSKxu/">
+            https://fb.watch/4v3OxhSKxu/
+          </Link>
+
+          <Link href="https://fb.watch/4v3P8Nhs2g/">
+            https://fb.watch/4v3P8Nhs2g/
+          </Link>
+
+          <img src="/javor_23.jpg" />
+          <img src="/javor_24.jpg" />
+          <img src="/javor_25.jpg" />
+          <img src="/javor_26.jpg" />
+          <img src="/javor_27.jpg" />
+          <img src="/javor_28.jpg" />
+          <img src="/javor_29.jpg" />
+          <img src="/javor_30.jpg" />
+        </div>
+
+        <div>
+          <h3>Remízek živě: Javor žije! Koncert pro Javor </h3>
+          <Link href="https://fb.watch/4Ox8jGLg4S/">video</Link>
+          Youtube mix záznamů:
+          <Link href="https://youtu.be/RkarG697pzY ">Kittchen</Link>
+        </div>
+
+        <div>
+          <h3>Politická debata</h3>
+          <Link href="https://www.facebook.com/zachranjavor/videos/382899139398764">
+            video
+          </Link>
+        </div>
+
+        <div>
+          <h3>Magistrát Zastupitelstvo</h3>
+          <img src="/javor_38.jpg" />
+          <img src="/javor_39.jpg" />
+          <img src="/javor_40.jpg" />
+          <img src="/javor_41.jpg" />
+          <img src="/javor_42.jpg" />
+          <img src="/javor_43.jpg" />
+          <img src="/javor_44.jpg" />
+          <img src="/javor_45.jpg" />
+          <img src="/javor_46.jpg" />
+        </div>
+      </div>
     </div>
   );
 };
