32beddf2a9742741f431543c31ec585f3ca57461
Markéta Hornerová <hornerova.m@gmail.com>
2021-07-27 14:09:37 +0200

    Kolektiv

--- components/TeamContent/index.js
+++ components/TeamContent/index.js
@@ -1,22 +1,25 @@
+import styles from '../../styles/Team.module.css';
 import { contacts } from '../../data/contacts';
 
 const TeamContent = () => {
   // console.log(contacts);
 
   return (
-    <div className="container_page">
-      <h1>Kolektiv umělců při remízku</h1>
+    <section className={styles.team}>
+      <div className={styles.containerTeam}>
+        <h1>KOLEKTIV UMĚLCŮ PŘI&nbsp;REMÍZKU</h1>
 
-      {contacts.map((contact) => {
-        return (
-          <div key={contact.id}>
-            <p>Jméno: {contact.name}</p>
-            <p>E-mail: {contact.contact}</p>
-            <br />
-          </div>
-        );
-      })}
-    </div>
+        {contacts.map((contact) => {
+          return (
+            <div className={styles.contact} key={contact.id}>
+              <p>{contact.name}</p>
+              {/* <p>E-mail: {contact.contact}</p> */}
+              <div className={styles.line}></div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -37,9 +37,9 @@ export default function Home() {
       <div id="filosofie-spolku">
         <PhilosophyContent />
       </div>
-      <div id="blog">
+      {/* <div id="blog">
         <BlogContent />
-      </div>
+      </div> */}
 
       <Footer />
       {/* 
