302900aefe5cf53822c74f1d12ceb5292da4678c
Markéta Hornerová <hornerova.m@gmail.com>
2021-07-28 16:48:30 +0200

    Uprava Team

--- pages/projekty/javor.js
+++ pages/projekty/javor.js
@@ -9,11 +9,13 @@ const Javor = () => {
       <div className={styles.simplePage}>
         <div className={styles.container}>
           <div>
-            <h1 className={styles.title}>Občan Javor</h1>
-            <p>
-              Aktualizovaný playlist videí z dílny Remízek k Javoru Stříbrnému
-              na Anenském trojúhelníku
-            </p>
+            <div className={styles.title}>
+              <h1>OBČAN JAVOR</h1>
+              <p>
+                Aktualizovaný playlist videí z dílny Remízek k Javoru Stříbrnému
+                na Anenském trojúhelníku
+              </p>
+            </div>
             <p>
               <Link href="https://www.facebook.com/zachranjavor/videos/1610047322526542/">
                 SPOT
@@ -25,47 +27,42 @@ const Javor = () => {
                 Reportáž - NEDEJ SE:
               </Link>
             </p>
-            <p>Strom roku:</p>
           </div>
 
           <div>
             <p>Remízek audiovizuální projekty k Javoru na Smetanově nábřeží </p>
-            <h2>REPORTÁŽE</h2>
+            <h2>Reportáže</h2>
             <p>(Remízek feat. Zachraň Javor) </p>
 
             <div>
               <h3>Díl nultý / Poslední nádech svobody</h3>
-              <Link href="https://fb.watch/4v2d_ZIPC5/ ">
-                https://fb.watch/4v2d_ZIPC5/
-              </Link>
-              <Link href="https://youtu.be/CsVcfmd80Po">
-                https://youtu.be/CsVcfmd80Po
-              </Link>
+              <p>
+                video:{' '}
+                <Link href="https://fb.watch/4v2d_ZIPC5/ ">Facebook</Link> /{' '}
+                <Link href="https://youtu.be/CsVcfmd80Po">Youtube</Link>
+              </p>
               <p>/ Kamera, střih: Kryštof /</p>
             </div>
 
             <div>
               <h3>Díl první / Přípravy na kácení </h3>
-              <Link href="https://fb.watch/4v3rxmYkH-/">
-                https://fb.watch/4v3rxmYkH-/
-              </Link>
-              <Link href="https://youtu.be/sDWskVhi3c8">
-                https://youtu.be/sDWskVhi3c8
-              </Link>
               <p>
-                / Kamera: Jan Trnka, Zvuk: Kryštof, Střih: Kryštof, Marcel
-                Halcin, Juliana Moska /
+                video: <Link href="https://fb.watch/4v3rxmYkH-/">Facebook</Link>{' '}
+                / <Link href="https://youtu.be/sDWskVhi3c8">Youtube</Link>
+                <p>
+                  / Kamera: Jan Trnka, Zvuk: Kryštof, Střih: Kryštof, Marcel
+                  Halcin, Juliana Moska /
+                </p>
               </p>
             </div>
 
             <div>
               <h3>Díl druhý / Bagr vs. Holub</h3>
-              <Link href="https://fb.watch/4v3xekd2bT/">
-                https://fb.watch/4v3xekd2bT/
-              </Link>
-              <Link href="https://youtu.be/wsMl3SRjnMc">
-                https://youtu.be/wsMl3SRjnMc
-              </Link>
+              <p>
+                video:
+                <Link href="https://fb.watch/4v3xekd2bT/">Facebook</Link> /{' '}
+                <Link href="https://youtu.be/wsMl3SRjnMc">Youtube</Link>
+              </p>
               <p>
                 / Kamera: Juliana Moska, Adéla Kaiserová, Kryštof, Zvuk: Adéla
                 Kaiserová, Matěj Lindner Střih: Rudi Mašata, Kryštof /
@@ -74,14 +71,14 @@ const Javor = () => {
 
             <div>
               <h3>Díl třetí / Otevřená radnice</h3>
-              <Link href="https://fb.watch/4Oxk_K1vbK/">
-                https://fb.watch/4Oxk_K1vbK/
-              </Link>
+              <p>
+                video: <Link href="https://fb.watch/4Oxk_K1vbK/">Facebook</Link>
+              </p>
             </div>
           </div>
 
           <div>
-            <h2>ŽIVÁ VYSÍLÁNÍ</h2>
+            <h2>Živá vysílání</h2>
             <div>
               <h3>Remízek živě: Bagrování Anenského trojúhelníku</h3>
               <Link href="https://fb.watch/4v3sy6rND0/">
@@ -110,7 +107,7 @@ const Javor = () => {
             <div>
               <h3>Na slovo u Javoru</h3>
               <Link href="https://www.facebook.com/TPSremizek/videos/935027017234833 ">
-                https://www.facebook.com/TPSremizek/videos/935027017234833
+                video
               </Link>
             </div>
 
@@ -121,32 +118,29 @@ const Javor = () => {
                 Živý střih: Miky Arsenjev, Tereza Schrijversová, Adéla
                 Kaiserová, Přenos: Kryštof, Zvuk: Nicolas Atcheson/
               </p>
-              <Link href="https://fb.watch/4v3OxhSKxu/">
-                https://fb.watch/4v3OxhSKxu/
-              </Link>
-
-              <Link href="https://fb.watch/4v3P8Nhs2g/">
-                https://fb.watch/4v3P8Nhs2g/
-              </Link>
+              <Link href="https://fb.watch/4v3OxhSKxu/">video 1</Link> /
+              <Link href="https://fb.watch/4v3P8Nhs2g/">video 2</Link>
             </div>
 
             <div>
               <h3>Remízek živě: Javor žije! Koncert pro Javor </h3>
               <Link href="https://fb.watch/4Ox8jGLg4S/">video</Link>
-              Youtube mix záznamů:
-              <Link href="https://youtu.be/RkarG697pzY ">Kittchen</Link>
+              <p>
+                Youtube mix záznamů:{' '}
+                <Link href="https://youtu.be/RkarG697pzY ">Kittchen</Link>
+              </p>
             </div>
 
-            <div>
+            {/* <div>
               <h3>Politická debata</h3>
               <Link href="https://www.facebook.com/zachranjavor/videos/382899139398764">
                 video
               </Link>
-            </div>
+            </div> */}
 
-            <div>
+            {/* <div>
               <h3>Magistrát Zastupitelstvo</h3>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
