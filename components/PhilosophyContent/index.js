32fd1c63d8ef4afca794b8d184ec9b2a7c403c62
Markéta Hornerová <hornerova.m@gmail.com>
2021-07-29 22:48:51 +0200

    uprava responziv

--- components/PhilosophyContent/index.js
+++ components/PhilosophyContent/index.js
@@ -1,6 +1,15 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 import styles from '../../styles/Philosophy.module.css';
 
 const PhilosophyContent = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
     <section className={styles.philosophy}>
       <div className={styles.containerPhilosophy}>
@@ -12,6 +21,15 @@ const PhilosophyContent = () => {
         </p>
         <button>Stanovy</button>
       </div>
+
+      {/* <div>
+        <Document file="stanovys.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div> */}
     </section>
   );
 };
